3c67bde577bd6eeb22088df5a2d304b78f330ca8
Ilia D <id@diler.ru>
2021-02-24 23:04:48 +0300

    clear repository + first push

---
+++ README.md
@@ -0,0 +1,16 @@
+# Разработчик на Spring Framework
+
+Страница курса [Перейти](https://otus.ru/lessons/javaspring/)
+
+## Месяц 1
+
+- Введение в Spring Framework:
+  [Домашнее задание](lesson1/src/main/java/home_work/)
+- Конфигурирование Spring-приложений
+- "Чёрная магия" Spring Boot
+- AOP, Spring AOP
+- Продвинутая конфигурация Spring-приложений
+- Разбор домашних заданий, QnA
+- DAO на Spring JDBC
+- Основы ORM, JPA, Hibernate как провайдер JPA
+- JPQL, Spring ORM, DAO на основе Spring ORM + JPA
---
+++ lesson1/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,17 @@
+package ru.otus.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        PersonService service = context.getBean(PersonService.class);
+        Person ivan = service.getByName("Ivan");
+
+        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+    }
+}
